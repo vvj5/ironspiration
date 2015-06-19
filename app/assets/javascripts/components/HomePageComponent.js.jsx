0becebaad8e4a0e1292888f365ada54fb90add44
Zach Chaney <zachary.chaney@gmail.com>
2015-06-19 15:26:17 -0500

    adding compoents

--- app/assets/javascripts/components/HomePageComponent.js.jsx
+++ app/assets/javascripts/components/HomePageComponent.js.jsx
@@ -1,17 +1,17 @@
 var HomePageComponent = React.createClass({
-    render: function () {
-        return (
-          <div>
-            <h1> Home Page </h1>
-            <a href='#login'>Login</a>
-            <a href='#register'>Register</a>
-            <a href='#pitch-form'>Submit pitch</a>
-            <a href='#project-form'>Submit Project</a>
-            <a href='#pitch-page'>List of Pitches</a>
-            <a href='#project-page'>Individual Projects</a>
-            <a href='#success'>Success Page</a>
-            <a href='#nsdnfjksndfn'>Page Not Found</a>
-          </div>
-        );
-    }
+	render: function () {
+		return (
+			<div>
+				<h1>Home Page </h1>
+				<a href='#home'>home</a>
+				<div>
+					<a href='#login'>Login</a>
+				</div>
+				<div>
+					<a href='#project-form'>Add Project</a>
+					<a href='#pitch-page'>Pitches</a>
+				</div>
+			</div>
+		);
+	}
 });
--- app/assets/javascripts/components/OverallProjectPageComponent.js.jsx
+++ app/assets/javascripts/components/OverallProjectPageComponent.js.jsx
@@ -1,10 +1,17 @@
 var OverallProjectPageComponent = React.createClass({
-    render: function () {
-        return (
-          <div>
-            <h1> Individual Project Page </h1>
-            <a href='#home'>home</a>
-          </div>
-        );
-    }
+	render: function () {
+		return (
+			<div>
+				<h1> Pitch Description Page </h1>
+				<a href='#home'>home</a>
+				<div>
+					<a href='#login'>Login</a>
+				</div>
+				<div>
+					<a href='#pitch-page'>Pitch List</a>
+					<a href='#home'>Projects List</a>
+				</div>
+			</div>
+		);
+	}
 });
