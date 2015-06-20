61cd7dbba531521d21930032105a9c35e2e8905e
Will Sherman <vvj5@icloud.com>
2015-06-20 14:45:36 -0500

    Error 400 attempted fix in Projects Controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -78,7 +78,7 @@ class ProjectsController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def project_params
-      params.require(:project).permit(:title, :body, :location, :image_link, :project_url, :year, :likes)
-    end
+    # def project_params
+    #   params.require(:project).permit(:title, :body, :location, :image_link, :project_url, :year, :likes)
+    # end
 end
