c0f6ba555f1433635a4057df3b64f25a30d4c5b5
Zach Chaney <zachary.chaney@gmail.com>
2015-06-21 15:24:06 -0500

    manually imported Rob's changes

--- app/assets/javascripts/components/ImageListComponent.js.jsx
+++ app/assets/javascripts/components/ImageListComponent.js.jsx
@@ -1,41 +1,48 @@
 var ImageListComponent = React.createClass({
-	getInitialState: function() {
-		return {
-			ImgThumbnailArray: []
-		}
-	},
-	componentDidMount: function() {
-		$.get (
-			"http://localhost:3000/projects",
-			function(imgList) {
-				if(this.isMounted()) {
-					this.setState({
-						ImgThumbnailArray: (imgList)
-					});
-				}
-			}.bind(this)
-		);
-	},
-	render: function () {
+    getInitialState: function() {
+        return {
+            ImgThumbnailArray: []
+        }
+    },
+    componentWillMount: function() {
+        ProjectList.on("add", this.postAdded);
+    },
+    componentDidMount: function() {
+        $.get (
+            "http://localhost:3000/projects",
+            function(imgList) {
+                if(this.isMounted()) {
+                    this.setState({
+                        ImgThumbnailArray: (imgList)
+                    });
+                }
+            }.bind(this)
+        );
+    },
+    render: function() {
 
-		thumbnails = this.state.ImgThumbnailArray.map(function(testModel){
-			
-			return (
-					<div className="col6" key ={testModel.id}>
-						<h1>{testModel.title}</h1>
-						<img src="https://vice-images.vice.com/images/articles/crops/2013/02/05/you-should-forgive-bill-murray-for-making-a-bad-movie-1413325188872-crop_mobile_400.jpeg?resize=*:*&output-quality=75" />
-					</div>	
-				);
-		});
-		return (
-			<div className="row" onClick = {this.linkToProjectPage}>
-				{thumbnails}
-			</div>
-		);
-	},
-
-	linkToProjectPage: function(e) {
-		console.log("test");
-		console.log(thumbnails[0].key);
-	}
+        thumbnails = this.state.ImgThumbnailArray.map(function(testModel){
+            
+            return (
+                    <div className="col6" ref="pic" key={testModel.id}>
+                        <h1>{testModel.title}</h1>
+                        <img data-id={testModel.id} src={testModel.image_link} />
+                    </div>
+                );
+            
+        });
+        return (
+            <div className="row" onClick = {this.linkToProjectPage}>
+                {thumbnails}
+            </div>
+        );
+    },
+    postAdded: function(postModel) {
+        this.forceUpdate();
+    },
+    linkToProjectPage: function(e) {
+        var dataID = $(e.target).attr("data-id");
+        console.log(dataID);
+        myRouter.navigate("project-page/"+dataID, {trigger:true});
+    }
 });
--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -2,62 +2,63 @@
 var ImgThumbnailArray =[];
 var ProjectList = new ProjectCollection();
 var App = Backbone.Router.extend({
-	routes: {
-		'': 'home',
-		"home": "home",
-		"login": "login",
-		'register': 'register',
-		"pitch-form": "pitchForm",
-		"project-form": "projectForm",
-		"pitch-page": "pitchPage",
-		"project-page": "projectPage",
-		"success": "success",
-		"*notfound": "dne"
-	},
-	home: function() {
-		React.render(
-			<HomePageComponent/>,
-			document.querySelector('#container')
-		);
-	},
-	login: function() {
-		React.render(<LoginPageComponent/>,
-		document.querySelector('#container'));
-	},
-	pitchForm: function() {
-		React.render(<PitchFormComponent/>,
-		document.querySelector('#container'));
-	},
-	register: function() {
-		React.render(<RegisterPageComponent/>,
-		document.querySelector('#container'));
-	},
-	projectForm: function() {
-		React.render(<ProjectFormComponent/>,
-		document.querySelector('#container'));
-	},
-	projectPage: function() {
-		React.render(
-		<div>
-			<OverallProjectPageComponent/>
-		</div>,
-		document.querySelector('#container'));
-	},
-	pitchPage: function() {
-		React.render(
-		<div>
-		   <OverallPitchPageComponent/>
-		</div>,
-		document.querySelector('#container'));
-	},
-	success: function() {
-		React.render(<SuccessPageComponent/>,
-		document.querySelector('#container'));
-	},
-	dne: function() {
-		React.render(<PageNotFoundComponent/>,
-		document.querySelector('#container'));
-	}
+    routes: {
+        '': 'home',
+        "home": "home",
+        "login": "login",
+        'register': 'register',
+        "pitch-form": "pitchForm",
+        "project-form": "projectForm",
+        "pitch-page": "pitchPage",
+        "project-page/:picture": "projectPage",
+        "success": "success",
+        "*notfound": "dne"
+    },
+    home: function() {
+        React.render(
+        <HomePageComponent/>,
+        document.querySelector('#container'));
+    },
+    login: function() {
+        React.render(
+        <LoginPageComponent/>,
+        document.querySelector('#container'));
+    },
+    pitchForm: function() {
+        React.render(
+        <PitchFormComponent/>,
+        document.querySelector('#container'));
+    },
+    register: function() {
+        React.render(
+        <RegisterPageComponent/>,
+        document.querySelector('#container'));
+    },
+    projectForm: function() {
+        React.render(
+        <ProjectFormComponent/>,
+        document.querySelector('#container'));
+    },
+    projectPage: function(picture) {
+        React.render(
+        <OverallProjectPageComponent picture={picture}/>,
+        document.querySelector('#container'));
+    },
+    pitchPage: function() {
+        React.render(
+        <OverallPitchPageComponent/>,
+        document.querySelector('#container'));
+    },
+    success: function() {
+        React.render(
+        <SuccessPageComponent/>,
+        document.querySelector('#container'));
+    },
+    dne: function() {
+        React.render(
+        <PageNotFoundComponent/>,
+        document.querySelector('#container'));
+    }
 });
 
 var myRouter = new App();
