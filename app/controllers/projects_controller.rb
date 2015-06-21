653a6770ab2ca9d006392c5232db08185583cd3a
Will Sherman <vvj5@icloud.com>
2015-06-20 20:08:06 -0500

    Updated Password and Auth settings across controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
-
+skip_before_action :verify_authenticity_token
   # GET /projects
   # GET /projects.json
   def index
