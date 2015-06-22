59092a75a89f49840bb282c43945d2c8fd540e2d
Rob <rdaly1490@gmail.com>
2015-06-21 21:24:48 -0500

    began addign comments

--- app/assets/javascripts/components/CommentListComponent.js.jsx
+++ app/assets/javascripts/components/CommentListComponent.js.jsx
@@ -4,12 +4,6 @@ var CommentListComponent = React.createClass({
 			<section>
 				<div className="comment-box">
 					<CommentFormComponent />
-					<p>This site rulezzz</p>
-					<p>Bill Murray is my spirit animal</p>
-					<p>Yummmm goldfish...</p>
-					<p>Dude. This is the best site Ie ever seen.</p>
-					<p>U rock 4real</p>
-					<p>I have nothing to say. So I won.</p>
 				</div>
 			</section>
 		);
