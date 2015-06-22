ad8d8e01b7d9ee4a7415a1d40fc8ec7340dc2c25
Your Name <you@example.com>
2015-06-21 21:14:09 -0500

    fixing style errors

--- app/assets/javascripts/components/CommentListComponent.js.jsx
+++ app/assets/javascripts/components/CommentListComponent.js.jsx
@@ -4,10 +4,10 @@ var CommentListComponent = React.createClass({
 			<section>
 				<div className="comment-box">
 					<CommentFormComponent />
-					<p>This site rulezzz</p>
+					<p>This site totes rulezzz yolo</p>
 					<p>Bill Murray is my spirit animal</p>
 					<p>Yummmm goldfish...</p>
-					<p>Dude. This is the best site Ie ever seen.</p>
+					<p>Dude. This is the best site I have ever seen.</p>
 					<p>U rock 4real</p>
 					<p>I have nothing to say. So I won.</p>
 				</div>
