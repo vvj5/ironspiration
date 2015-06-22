2e5ea757d902caad73c82e4c3ab71bdd14f86329
Zach Chaney <zachary.chaney@gmail.com>
2015-06-21 21:05:14 -0500

    fixing the shit outta shit

--- app/assets/javascripts/models/pitchModel.js
+++ app/assets/javascripts/models/pitchModel.js
@@ -9,7 +9,12 @@ var PitchedModel = Backbone.Model.extend({
     urlRoot: 'http://ironspiration.herokuapp.com/projects',
     idAttribute: '_id'
 
-    // validate: function(attr) {
-
-    // }
+    validate: function(attr, options) {
+        else if(attr.title.length === 0){
+            return 'A title must be present';
+        }
+        else if(attr.body.length === 0){
+            return 'A body must be present';
+        }
+        return false;
 });
